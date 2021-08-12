7d07d83576d6c88a89514445bf03507302da4de6
EdgarEspinozaL <edgarel161297@gmail.com>
2021-08-12 17:28:45 -0500

    actualizacion 8

--- js/carta.js
+++ js/carta.js
@@ -12,6 +12,13 @@ function back(){
     history.go(-1);
 }
 
+function limpiar(product, price){
+    console.log(products,price);
+    products.splice(product);
+    total=0;
+    document.getElementById("checkout").innerHTML = `Pagar s/${total}`
+}
+
 function pay() {
     window.alert(products.join("  \n")+ "\nTOTAL A PAGAR ES :"+ total );
 }
